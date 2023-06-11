8cd68cfce1e7518554f804fcb16263528459e932
Lukas Maile <lukasmaile@icloud.com>
2023-06-11 15:38:02 +0200

    Initial Commit

---
+++ TexAdventure/TexAdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TexAdventureApp.swift
+//  TexAdventure
+//
+//  Created by Lukas Maile on 11.06.23.
+//
+
+import SwiftUI
+
+@main
+struct TexAdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

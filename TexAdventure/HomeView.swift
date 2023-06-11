35b88ab830de95d3ecdba41aa29fbdb8a4726692
Lukas Maile <lukasmaile@icloud.com>
2023-06-11 18:14:47 +0200

    test

---
+++ TexAdventure/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  HomeView.swift
+//  TexAdventure
+//
+//  Created by Lukas Maile on 11.06.23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView{
+        VStack{
+            Spacer()
+            Text("Text Adventure Game")
+                .padding()
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Spacer()
+            
+            
+            
+                NavigationLink(destination: StoryView()) {
+                    Text("Start the Game")
+                        .controlSize(.large)
+                        .padding()
+                        .background(Capsule().foregroundColor(.blue))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- TexAdventure/TexAdventureApp.swift
+++ TexAdventure/TexAdventureApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TexAdventureApp: App {
     var body: some Scene {
         WindowGroup {
-            StoryView()
+            //StoryView()
+            HomeView()
         }
     }
 }
